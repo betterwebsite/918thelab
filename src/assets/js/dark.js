e5fa3fb2bd6dd89fc91ce2dca4767f50e4e6a182
Dfletcher198 <david@davidfletcherrealtor.com>
2025-06-09 18:31:54 -0500

    demo
    demo

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -1,39 +1,10 @@
-function togglePlayButton() {
-    // Select all elements with the .cs-picture class
-    const pictures = document.querySelectorAll('#hero-2219 .cs-video-group');
-  
-    // Add a click event listener to each .cs-picture element
-    pictures.forEach(picture => {
-      picture.addEventListener('click', () => {
-        // Select all elements with the .cs-play class
-        const playButtons = document.querySelectorAll('#hero-2219 .cs-play');
-        
-        // Toggle the .cs-hide class on each .cs-play element
-        playButtons.forEach(button => {
-          button.classList.toggle('cs-hide');
+const toggle = document.querySelector("#pricing-700 .cs-toggle");
+        const cardGroup = Array.from(document.querySelectorAll('.cs-card-group'))
+
+        toggle.addEventListener('click', (e) => { 
+            for (const item of cardGroup) {
+                item.classList.toggle("active");
+            }
+            toggle.classList.toggle("active");
         });
-      });
-    });
-  }
-  
-  // Call the function to activate the event listeners
-  togglePlayButton();
-  
-  function toggleVideoPlayback() {
-    // Select the video element
-    const video = document.querySelector('#hero-2219 video');
-  
-    // Add a click event listener to the video
-    video.addEventListener('click', () => {
-      // Check if the video is paused
-      if (video.paused) {
-        video.play(); // Play the video if it is paused
-      } else {
-        video.pause(); // Pause the video if it is playing
-      }
-    });
-  }
-  
-  // Call the function to activate the event listener
-  toggleVideoPlayback();
-                                
+                                   
